8a193c75a052e99e3349efdb97f15c3e09aea983
claudiolutz <claudio.lan@hotmail.com>
2025-07-20 18:12:04 +0200

    run_migrations.py too run the migration

--- run_migrations.py
+++ run_migrations.py
@@ -3,8 +3,3 @@ from flask_migrate import Migrate
 
 app = create_app()
 migrate = Migrate(app, db)
-
-with app.app_context():
-    # This script is just for setting up the context.
-    # You still need to run the flask db commands in your terminal.
-    print("Flask app context created. Run 'flask db migrate' and 'flask db upgrade'.")
